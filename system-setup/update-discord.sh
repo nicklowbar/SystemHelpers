b20d0fb8db78e28b99a619e78cc157e026ce47e0
Nicholas Barlow <nicklowbar@gmail.com>
2025-08-19 21:54:42 -0700

    rewrite update-discord to be cmdline invokable, and work without user intervention

--- system-setup/update-discord.sh
+++
@@ -1,7 +0,0 @@
-# note: This script is intended to be used whenever opening discord results in the client attempting to download an update .deb.
-# It would be better if we could check for updates on a schedule, but that requires some way to query Discord's API.
-# This script will attempt to download the latest version of the client and install it.
-
-wget "https://discord.com/api/download/stable?platform=linux&format=deb" -O /tmp/discord.deb
-sudo dpkg -i /tmp/discord.deb
-rm /tmp/discord.deb
